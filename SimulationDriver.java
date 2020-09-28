cc12f93e0bfc98b853473639a66e33857307f630
Tyler <35055803+trstamp@users.noreply.github.com>
2020-09-28 14:39:24 -0700

    Update SimulationDriver.java
    add the methods from voting service so the driver can automate responses

--- SimulationDriver.java
+++ SimulationDriver.java
@@ -6,7 +6,9 @@ public class SimulationDriver {
         Random rd = new Random(); //random object
         VotingService sim = new VotingService(); // One simulation
 
-
-
+        sim.addQuestions(); // add questions into the list
+        sim.addStudents(); // add students
+        sim.answerQuestions(); // students answer each question
+        sim.printStatistics(); // print results of each question
     }
 }
