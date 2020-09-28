4a201b7df489d3baacfe509e7d8f5aa053552869
Tyler <35055803+trstamp@users.noreply.github.com>
2020-09-28 02:40:30 -0700

    Create SimulationDriver.java
    Driver for the Voting Simulator

---
+++ SimulationDriver.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class SimulationDriver {
+
+    public static void main(String[] args){
+        Random rd = new Random(); //random object
+        VotingService sim = new VotingService(); // One simulation
+
+
+
+    }
+}
